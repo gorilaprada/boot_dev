1943bdc9d14f4207f6276f2e0b2caf0f2927c6d0
gorilaprada <carlosprada40@outlook.com>
2026-05-15 12:04:05 -0500

    10 Ch3 L1: Done

--- 10_static_site_generator/src/test_textnode.py
+++ 10_static_site_generator/src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType, text_node_to_html_node
+from split_nodes_delimiter import split_nodes_delimiter
 
 class testTextNode(unittest.TestCase):
     def test_eq(self):
@@ -42,6 +43,61 @@ class testTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.props, { "href": "www.link.com" })
 
+    # Test function split_nodes_delimiter
+    def test_text(self):
+        node = TextNode("This is a text node with some `def foo(): return` and more `def foo2(): return`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        node2 = TextNode("def foo(): return", TextType.CODE)
+        node3 = TextNode("def foo2(): return", TextType.CODE)
+        self.assertEqual(new_nodes[1], node2)
+        self.assertEqual(new_nodes[3], node3)
+
+    def test_full_output(self):
+        node = TextNode("This is a text node with some _italic1_ and more _italic2_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        node0 = TextNode("This is a text node with some ", TextType.TEXT)
+        node1 = TextNode("italic1", TextType.ITALIC)
+        node2 = TextNode(" and more ", TextType.TEXT)
+        node3 = TextNode("italic2", TextType.ITALIC)
+        self.assertEqual(new_nodes, [node0, node1, node2, node3])
+
+    def test_full_output2(self):
+        node = TextNode("This is a text node with some **bold1** and **bold2** more ", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        node0 = TextNode("This is a text node with some ", TextType.TEXT)
+        node1 = TextNode("bold1", TextType.BOLD)
+        node2 = TextNode(" and ", TextType.TEXT)
+        node3 = TextNode("bold2", TextType.BOLD)
+        node4 = TextNode(" more ", TextType.TEXT)
+        self.assertEqual(new_nodes, [node0, node1, node2, node3, node4])
+
+    def test_two_nodes(self):
+        big_node = TextNode("This is a text node", TextType.TEXT)
+        big_node2 = TextNode("This is a text node with some code: `code`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([big_node, big_node2], "`", TextType.CODE)
+        node0 = TextNode("This is a text node", TextType.TEXT)
+        node1 = TextNode("This is a text node with some code: ", TextType.TEXT)
+        node2 = TextNode("code", TextType.CODE)
+        self.assertEqual(new_nodes, [node0, node1, node2])
+
+    def test_node_with_type_CODE(self):
+        big_node = TextNode("`def foo(): return`", TextType.CODE)
+        new_nodes = split_nodes_delimiter([big_node], "`", TextType.CODE)
+        node0 = TextNode("`def foo(): return`", TextType.CODE)
+        self.assertEqual(new_nodes[0], node0)
+
+    def test_delim_bold_and_italic(self):
+        node = TextNode("**bold** and _italic_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+        self.assertListEqual(
+            [
+                TextNode("bold", TextType.BOLD),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+            ],
+            new_nodes,
+        )
 
 if __name__ == "__main__":
     unittest.main()
