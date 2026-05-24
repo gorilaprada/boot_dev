c98b08fcac981768dcf8c4b3a619d84e304a48bc
gorilaprada <carlosprada40@outlook.com>
2026-05-23 19:17:46 -0500

    10 Chap5 L1: Done

--- 10_static_site_generator/src/main.py
+++ 10_static_site_generator/src/main.py
@@ -1,9 +1,31 @@
 from textnode import TextNode, TextType, text_node_to_html_node
 from htmlnode import HTMLNode, LeafNode, ParentNode
+import os
+import shutil
 
 def main():
-    testNode = TextNode("some text", TextType.TEXT,"www.gorilaprada.com")
-    print(testNode)
+    src = "./static/"
+    target = "./public/"
+    if os.path.exists(target):
+        shutil.rmtree(target)
+    static_to_public(src, target)
+
+
+def static_to_public(src: str, target: str) -> None:
+    if not os.path.exists(target):
+        os.mkdir(target)
+    list_of_dir = os.listdir(src)
+    for dir in list_of_dir:
+        new_src = os.path.join(src, dir)
+        new_target = os.path.join(target, dir)
+        if os.path.isfile(new_src):
+            shutil.copy(new_src, new_target)
+            print(f"{new_src} copied to {new_target}")
+        else:
+            static_to_public(new_src, new_target)
+    return
+
+
 
 
 main()
